655c485fc69e40a6deab27f290e78e4aa6217070
KYC5027 <fkdlej201@gmail.com>
2025-04-28 17:45:41 +0900

    Update CarRepairDAO.java

--- src/main/java/com/boot/dao/CarRepairDAO.java
+++ src/main/java/com/boot/dao/CarRepairDAO.java
@@ -1,12 +1,5 @@
 package com.boot.dao;
 
 public class CarRepairDAO {
-	List<CarRepairShopDTO> getAllRepairShops();
 
-    List<CarRepairShopDTO> getRepairShopsInBounds(
-            @Param("swLat") double swLat,
-            @Param("swLng") double swLng,
-            @Param("neLat") double neLat,
-            @Param("neLng") double neLng
-        );
 }
