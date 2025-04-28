72539366418f3bcb98c25428ef54e2aca3351901
KYC5027 <fkdlej201@gmail.com>
2025-04-28 17:44:48 +0900

    api 데이터 , 좌표

--- src/main/java/com/boot/dao/CarRepairDAO.java
+++ src/main/java/com/boot/dao/CarRepairDAO.java
@@ -1,5 +1,12 @@
 package com.boot.dao;
 
 public class CarRepairDAO {
+	List<CarRepairShopDTO> getAllRepairShops();
 
+    List<CarRepairShopDTO> getRepairShopsInBounds(
+            @Param("swLat") double swLat,
+            @Param("swLng") double swLng,
+            @Param("neLat") double neLat,
+            @Param("neLng") double neLng
+        );
 }
