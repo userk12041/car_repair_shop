d453cafa86b1eba7e3d6a4d6ebc1b28f26e7c5b2
KYC5027 <fkdlej201@gmail.com>
2025-04-29 09:36:40 +0900

    Update CarRepairDTO.java

--- src/main/java/com/boot/dto/CarRepairDTO.java
+++ src/main/java/com/boot/dto/CarRepairDTO.java
@@ -4,22 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarRepairDTO {
 	
-	@Data
-	@AllArgsConstructor
-	@NoArgsConstructor
-	public class RepairShopDTO {
-	    	private int id;
-		
-		private String name;
-		private String roadaddress;
-		private String lotaddress;
-		private String latitude;
-		private String longitude;
-		private String registration_date;
-		private String opentime;
-		private String closetime;	
-		private String telnumber;	
-	}
+	private int id;
+	
+	private String name;
+	private String roadaddress;
+	private String lotaddress;
+	private String latitude;
+	private String longitude;
+	private String registration_date;
+	private String opentime;
+	private String closetime;	
+	private String telnumber;	
+	
 }
