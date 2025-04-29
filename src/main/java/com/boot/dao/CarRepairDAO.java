e55a838c10d4dd29e9dbcd0254c98a11e263a0f6
KYC5027 <fkdlej201@gmail.com>
2025-04-29 09:36:53 +0900

    Update CarRepairDAO.java

--- src/main/java/com/boot/dao/CarRepairDAO.java
+++ src/main/java/com/boot/dao/CarRepairDAO.java
@@ -1,5 +1,21 @@
 package com.boot.dao;
 
-public class CarRepairDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.boot.dto.CarRepairDTO;
+
+@Mapper
+public interface CarRepairDAO {
+	List<CarRepairDTO> getAllRepairShops();
+	
+	
+    List<CarRepairDTO> getRepairShopsInBounds(
+            @Param("swLat") double swLat,
+            @Param("swLng") double swLng,
+            @Param("neLat") double neLat,
+            @Param("neLng") double neLng
+        );
 }
