f0a3ae5a5a4713b1555f37373b7e335d56663e47
KYC5027 <fkdlej201@gmail.com>
2025-04-29 09:37:57 +0900

    Update PageController.java

--- src/main/java/com/boot/controller/PageController.java
+++ src/main/java/com/boot/controller/PageController.java
@@ -1,13 +1,35 @@
 package com.boot.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class PageController {
+import com.boot.dao.CarRepairDAO;
+import com.boot.dto.CarRepairDTO;
 
-    @GetMapping("/map")
-    public String mapPage() {
-        return "map";  // /WEB-INF/views/map.jsp 로 이동
+@RestController
+@RequestMapping("/api/repairShops")
+public class CarRepairApiController {
+
+    @Autowired
+    private CarRepairDAO carRepairShopDAO;
+
+    @GetMapping
+    public List<CarRepairDTO> getAllRepairShops() {
+        return carRepairShopDAO.getAllRepairShops();
+    }
+    
+    @GetMapping("/api/repairshops")
+    public List<CarRepairDTO> getRepairShopsInBounds(
+            @RequestParam double swLat,
+            @RequestParam double swLng,
+            @RequestParam double neLat,
+            @RequestParam double neLng) {
+
+        return carRepairShopDAO.getRepairShopsInBounds(swLat, swLng, neLat, neLng);
     }
 }
