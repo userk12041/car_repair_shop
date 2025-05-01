77b5ad95bdd3cd37637610e02312d14a2ae67384
userk12041 <ikjh1106@naver.com>
2025-05-01 12:11:04 +0900

    업체 정보페이지 리뷰 추가

--- src/main/java/com/boot/controller/CarRepairPageController.java
+++ src/main/java/com/boot/controller/CarRepairPageController.java
@@ -1,5 +1,8 @@
 package com.boot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.boot.dao.CarRepairDAO;
 import com.boot.dto.CarRepairDTO;
+import com.boot.dto.ReviewDTO;
+import com.boot.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,10 +22,14 @@ import lombok.RequiredArgsConstructor;
 public class CarRepairPageController {
 
     private final CarRepairDAO carRepairDAO;
+    @Autowired
+    private ReviewService reviewService;
 
     @GetMapping("/view")
     public String detailPage(@RequestParam("id") int id, Model model) {
         CarRepairDTO shop = carRepairDAO.getRepairById(id);
+        List<ReviewDTO> reviews = reviewService.getReviewsByShopId(id);
+        model.addAttribute("reviews", reviews);
         model.addAttribute("shop", shop);
         return "view";  // view.jsp 로 이동
     }
